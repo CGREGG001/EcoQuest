fd4ca744464badb7764b73280ff21c2b4915be2a
Gregory <cgregg@belgacom.net>
2025-12-29 23:24:28 +0100

    refactor(configurations): update GroupConfig to support GroupType and InvitationCode VO

--- EcoQuest.Web/Data/Configurations/GroupConfig.cs
+++ EcoQuest.Web/Data/Configurations/GroupConfig.cs
@@ -1,4 +1,4 @@
-using EcoQuest.Web.Models.Domain;
+using EcoQuest.Domain.ValueObjects;
 using EcoQuest.Web.Models.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -14,19 +14,31 @@ public class GroupConfig : IEntityTypeConfiguration<Group>
         builder.HasKey(g => g.Id)
             .HasName("PK_Groups");
 
+        // --- InvitationCode (Value Object) ---
+        builder.Property(g => g.InvitationCode)
+            .HasConversion(
+                ic => ic.Value,                          // VO → string (DB)
+                ic => InvitationCode.Create(ic))    // string → VO
+            .HasColumnName("InvitationCode")
+            .HasMaxLength(8)
+            .IsRequired();
+
         builder.HasIndex(g => g.InvitationCode)
             .IsUnique();
 
+        // --- Name ---
         builder.Property(g => g.Name)
             .HasMaxLength(100)
             .IsRequired();
 
+        // --- GroupType (enum) ---
         builder.Property(g => g.Type)
-            .HasMaxLength(100)
-            .IsRequired();
-
-        builder.Property(g => g.InvitationCode)
+            .HasConversion<string>()   // stocké en string dans la DB
             .HasMaxLength(50)
             .IsRequired();
+
+        // --- Users relation ---
+        builder.HasMany(g => g.Users)
+            .WithMany(u => u.Groups); // si tu as la relation Many-to-Many
     }
 }
